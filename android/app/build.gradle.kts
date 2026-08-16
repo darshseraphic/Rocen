2f2cb577b6674aa3e9a053f22528911eabd6b576
darshseraphic <darsh.seraphic@gmail.com>
2026-08-17 01:24:16 +0530

    initial commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,6 +35,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk {
+                debugSymbolLevel = "NONE"
+            }
         }
     }
 }
